8a7c38a8bce1ae9f251b0b2825aae0e81de15794
joshuanaibaho-source <joshuanaibaho799@gmail.com>
2026-08-31 09:48:08 +0700

    Menambahkan output biodata

--- src/Biodata.java
+++ src/Biodata.java
@@ -6,5 +6,11 @@ public class Biodata {
         String asal_daerah = "Bogor";
         String alasan = "Karna saya mau jadi data analyst";
         String target = "Mendapatkan GPA 4.0";
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Prodi : " + prodi);
+        System.out.println("Asal Daerah : " + asal_daerah);
+        System.out.println("Alasan : " + alasan);
+        System.out.println("Target : " + target);
     }
 }
