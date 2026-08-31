f75aa8e7ad09755d1917a53842b20db6aee234a2
joshuanaibaho-source <joshuanaibaho799@gmail.com>
2026-08-31 09:40:03 +0700

    Menambahkan data biodata

--- src/Biodata.java
+++ src/Biodata.java
@@ -1,5 +1,10 @@
 public class Biodata {
     public static void main(String[] args) {
-
+        String nama = "Joshua Fowler Raynaldo Naibaho";
+        String nim = "2582013";
+        String prodi = "Sistem Informasi";
+        String asal_daerah = "Bogor";
+        String alasan = "Karna saya mau jadi data analyst";
+        String target = "Mendapatkan GPA 4.0";
     }
 }
